39532c9e4fd43248c025e97adfe1c9ebc0061c8d
Idel Montalvo <idel_m@hotmail.com>
2024-12-20 10:22:29 +0100

    First Commit

---
+++ Dias_restantes.py
@@ -0,0 +1,12 @@
+#Calcula los dias restantes para una fecha futura especifica
+
+from datetime import datetime
+
+fecha = input("Feccha (formato: AAAA-MM-DD): ")
+fecha = datetime.strptime(fecha,"%Y-%m-%d") 
+
+
+hoy = datetime.now()
+dias_restantes = (fecha - hoy).days
+
+print(f"Faltan {dias_restantes}")
